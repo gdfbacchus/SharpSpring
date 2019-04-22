40633f0067839bf9a6481952e0a6cf0cda9156af
The Walrus <gdfbacchus@gmail.com>
2019-04-22 02:14:04 -0400

    init

---
+++ src/Components/albums.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import Moment from 'react-moment'
+const axios = require('axios')
+
+export default class Albums extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            albums: []
+        };
+    }
+    componentDidMount() {
+        axios.get(`https://itunes.apple.com/search?term=cage+the+elephant&entity=album&callback=`)
+            .then(res => {
+                this.setState({
+                    albums: res.data.results});
+            })
+            .catch(err => [
+                console.log(err)
+            ]);
+            
+    };
+    render() {
+        return (
+            <div className="row">
+                {this.state.albums.map(album =>(
+                    <div className="col-md-4 col-sm-6 card" key={album.collectionId}>
+                        <img src={album.artworkUrl100} className="card-img-top" alt={album.collectionName} />
+                        <div className="card-body">
+                            <h5 className="card-tile">{album.collectionName}</h5>
+                            <p className="card-text"><Moment format="MMM D YYYY">{album.releaseDate}</Moment></p>
+                        </div>
+                        
+                    </div> 
+                ))}
+            </div>
+        )
+    }
+}            
---
+++ src/Components/title.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Title extends React.Component {
+    render() {
+        return (
+            <div className="App-header">
+                <img src="https://i.pinimg.com/originals/3e/33/66/3e3366e69358cf30ce192151b05d3f46.png" alt="image1" />
+                <h2>Cage the Elephant Discography</h2>
+            </div>
+        );
+    }
+}
+
+export default Title;
